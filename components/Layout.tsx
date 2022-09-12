faf8a4509d742fee0e7bc8e0e84856a2c77d73f1
chspower1 <chspower1@gmail.com>
2022-09-13 01:30:59 +0900

    head title 설정 완료

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,16 @@
 import { PropsWithChildren } from "react";
 import NavBar from "./NavBar";
-
+import { useRouter } from "next/router";
+import Head from "next/head";
 export default function Layout({ children }: PropsWithChildren) {
+    const { pathname } = useRouter();
+    console.log(pathname);
+    const titleName = pathname === "/" ? "HOME" : pathname.slice(1).toUpperCase();
     return (
         <>
+            <Head>
+                <title>{titleName} | NEXT MOVIES</title>
+            </Head>
             <NavBar />
             <div>{children}</div>
         </>
