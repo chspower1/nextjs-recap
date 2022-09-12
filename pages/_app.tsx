73c1014152820c3738d1a02843aaa7cc76c6ca41
chspower1 <chspower1@gmail.com>
2022-09-13 01:06:23 +0900

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import NavBar from "../components/NavBar";
+import { AppProps } from "next/dist/shared/lib/router/router";
+import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
-
-export default MyApp
