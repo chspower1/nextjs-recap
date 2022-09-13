5162b9f5efde18f5fec8b74fcb141b4ce2e42298
chspower1 <chspower1@gmail.com>
2022-09-13 19:04:19 +0900

    pause

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,15 +3,25 @@ import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const Nav = styled.nav`
+    z-index: 2000;
+    position: fixed;
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     width: 100%;
     height: 50px;
-    background-color: gray;
     font-size: 16px;
 `;
+const Logo = styled.div`
+    margin-left: 100px;
+    font-size: 28px;
+`;
+const Menu = styled.div`
+    margin-right: 100px;
+    font-size: 28px;
+`;
 const NavBtn = styled.button`
+    margin: 0px 20px;
     &:hover {
         color: red;
     }
@@ -25,16 +35,21 @@ export default function NavBar() {
     return (
         <Nav>
             <Link href="/">
-                <NavBtn className={pathname === "/" ? "active" : "none"}>Home</NavBtn>
+                <Logo>CINEMA</Logo>
             </Link>
-            <Link href="/about">
-                <NavBtn className={pathname === "/about" ? "active" : "none"}>About</NavBtn>
-            </Link>
-            <Link href="/movies/popular">
-                <NavBtn className={pathname === "/movies/popular" ? "active" : "none"}>
-                    Popular
-                </NavBtn>
-            </Link>
+            <Menu>
+                <Link href="/">
+                    <NavBtn className={pathname === "/" ? "active" : "none"}>Home</NavBtn>
+                </Link>
+                <Link href="/about">
+                    <NavBtn className={pathname === "/about" ? "active" : "none"}>About</NavBtn>
+                </Link>
+                <Link href="/movies/popular">
+                    <NavBtn className={pathname === "/movies/popular" ? "active" : "none"}>
+                        Popular
+                    </NavBtn>
+                </Link>
+            </Menu>
         </Nav>
     );
 }
